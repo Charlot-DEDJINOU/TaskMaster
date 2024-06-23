dd2231cdcf842476f983cc7eeb45f9a31406c504
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-06-23 13:31:38 +0100

    package json

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [number, setNumber] = useState(0)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className='flex-1 items-center justify-center bg-green-700'>
+      <Text className='mx-2 text-2xl font-bold text-center text-white'>Charlot  a mis en place Tailwind CSS en React Native</Text>
+      <Text className='mt-3 text-white text-2xl'>{number}</Text>
+      <Button title='Incrémentation' onPress={() => setNumber(number + 1)}/>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
